2256f7d239d371f2e93905fbeedfaad90582b435
Justin Du <JDU@MacBook-Pro.local>
2022-08-14 01:24:24 -0400

    hello

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import './App.css';
 import Deck from "./deck.js"
 
+//const myCardOne = document.querySelector(".myCardOne")
+
 const deck = new Deck()
+deck.shuffle()
 console.log(deck.cards)
+
+//myCardOne.appendChild(deck.cards[0].getHTML())
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a>
-          Poker Yay
-        </a>
-      </header>
+    <div>
+      <div className="reference"> </div>
+      {/* <div className="myCardOne"> Card One</div> */}
+
+      
+      <div className="myCardTwo"> Card Two</div>
+        <div className="myCardTwo red" data-value="9♥">
+          ♥
+        </div>
+
+      <div className="myCardOne"></div>
+        <div className="myCardOne red" data-value="9♥">
+          ♥
+        </div>
+
+
+      {/* <div className="computer-deck deck">26</div>
+      <div className="computer-card-slot card-slot"></div>
+        <div className="card red" data-value="9♥">
+          ♥
+        </div>
+      <div className="text">Text</div>
+      <div className="player-deck deck">27</div>
+      <div className="player-card-slot card-slot"></div> */}
+      <div class = "lightRed"></div>
+      <div class = "white"></div>
+      <div class = "darkRed"></div>
+      <div class = "circle"></div>
     </div>
+
   );
 }
-
-//Testing 
-//Jason testing
 
 export default App;
--- src/deck.js
+++ src/deck.js
@@ -5,6 +5,22 @@ export default class Deck {
     constructor(cards = freshDeck()) { //pass into the constructor
         this.cards = cards
     }
+
+    //getter to get number of cards
+    get numberOfCards() { 
+        return this.cards.length
+    }
+
+    //randomly shuffles the arrays, like a leet code question
+    shuffle() {
+        this.cards.sort((a,b)=> Math.random()-0.5)
+        // for (let i = this.numberOfCards - 1; i > 0; i--) {
+        //     const newIndex = Math.floor(Math.random() * (i + 1)) //Random index before the current card
+        //     const oldValue = this.cards[newIndex] //Swap current card with old card that we had 
+        //     this.cards[newIndex] = this.cards[i] //L oop through all cards and swapping them with a card
+        //     this.cards[i] = oldValue
+        // }
+    }
 }
 
 class Card {
@@ -12,7 +28,19 @@ class Card {
         this.suit = suit
         this.value = value
     }
+
+    // get color() {
+    //     return this.suit === "♣" || this.suit === "♠" ? 'black' : 'red'
+    // }
+
+    // getHTML() {
+    //     const cardDiv = document.createElement('div')
+    //     cardDiv.innerText = this.suitcardDiv.classList.add("card", this.color)
+    //     cardDiv.dataset.value = `${this.value} ${this.suit}`
+    //     return cardDiv
+    // }
 }
+
 
 function freshDeck() {
     return SUITS.flatMap(suit =>{
